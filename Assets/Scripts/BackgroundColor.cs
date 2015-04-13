d571686c487dee79b912b9ea8361678b75156364
Paul Maurer <maurer.paul@ymail.com>
2015-04-13 20:06:21 +0200

    SALU le menu du swag est swag swagswag swaggy swag

---
+++ Assets/Scripts/BackgroundColor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundColor : MonoBehaviour {
+
+	public Color lerpedColor = Color.blue;
+	void Update() {
+		lerpedColor = Color.Lerp(Color.blue, Color.cyan, Time.time);
+		GetComponent<Renderer> ().material.color = lerpedColor;
+	}
+}
