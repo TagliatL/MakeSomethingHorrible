9f0aaf3da710bbf082a18c5b40e456bf59518a54
Paul Maurer <maurer.paul@ymail.com>
2015-05-08 00:18:21 +0200

    options de menu d'échec de mission qui marcheeeeeent

---
+++ Assets/Scripts/GUI/Button.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	void Retry() {
+		Application.LoadLevel(Application.loadedLevelName);
+	}
+
+	void Menu() {
+		Application.LoadLevel ("MenuLevels");
+	}
+}
