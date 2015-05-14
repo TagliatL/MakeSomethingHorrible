4b47538288096bc6043991563013540861199125
Mahrez Bendali <mahrez.bendali@gmail.com>
2015-05-14 23:42:20 +0200

    qsdgdf

---
+++ Assets/Scripts/GUI/GameMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMusic : MonoBehaviour {
+
+	void Awake ()
+	{
+		GameObject menuMusic = GameObject.Find("MenuMusic");
+		if (menuMusic)
+		{
+			Destroy(menuMusic);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
