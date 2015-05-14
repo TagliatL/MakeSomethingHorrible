3091fe423bd943b383b5cf640f1cb61e3ff8981f
Paul Maurer <maurer.paul@ymail.com>
2015-05-14 18:41:53 +0200

    PERLE DE LAIT CE N'EST PAS UN YAOURT

---
+++ Assets/Scripts/GUI/LevelsUnlock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelsUnlock : MonoBehaviour {
+
+	public GameObject[] levels;
+	public GameObject[] lockedLevels;
+
+	void Start () {
+		for (int i = 0; i < PlayerPrefs.GetInt ("NumberOfLevelWon"); i++) {
+			lockedLevels[i].gameObject.SetActive (false);
+			levels[i].gameObject.SetActive (true);
+		}
+	}
+}
